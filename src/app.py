283458ff5afa7b8fc74dc2cd3f13d09b3f9a7378
Diogo Santos <snackk026@gmail.com>
2025-08-11 01:21:00 +0100

    wake endpoint;

--- src/app.py
+++ src/app.py
@@ -81,6 +81,12 @@ def send_wol():
     send_magic_packet(MAC, ip_address=IP)
     return redirect(url_for('index'))
 
+@app.route("/wake", methods=["POST"])
+@requires_auth
+def wake():
+    send_magic_packet(MAC, ip_address=IP)
+    return '', 200
+
 @app.route("/health", methods=["GET"])
 def health_check():
     health_data = {
